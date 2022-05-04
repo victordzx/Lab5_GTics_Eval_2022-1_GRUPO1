323ad974c0b7d37f068f7b9b30e5677cf0d65002
Stephania <stephania.guzman@pucp.edu.pe>
2022-05-03 20:34:08 -0500

    JuegosRepository COMPLETO

--- Lab5_GTics_Eval_2022-1/src/main/java/edu/pucp/gtics/lab5_gtics_20221/repository/JuegosRepository.java
+++ Lab5_GTics_Eval_2022-1/src/main/java/edu/pucp/gtics/lab5_gtics_20221/repository/JuegosRepository.java
@@ -14,10 +14,13 @@ import java.util.List;
 
 public interface JuegosRepository extends JpaRepository<Juegos,Integer> {
     @Query(value = "Select  j.idjuego, j.nombre, j.descripcion, g.nombre as genero, j.image as imageURL from gameshop3.juegos j " +
-            "inner join gameshop3.juegosxusuario ju  on j.idjuego=ju.idjuego " +
-            "inner join gameshop3.usuarios u on ju.idusuario=u.idusuario " +
-            "inner join gameshop3.generos g on g.idgenero=j.idgenero Where u.idusuario= ?",nativeQuery = true)
+            "inner join gameshop4.juegosxusuario ju  on j.idjuego=ju.idjuego " +
+            "inner join gameshop4.usuarios u on ju.idusuario=u.idusuario " +
+            "inner join gameshop4.generos g on g.idgenero=j.idgenero Where u.idusuario= ?",nativeQuery = true)
     List<JuegosUserDto> obtenerJuegosPorUser(int idusuario);
+
+    @Query(value="SELECT * FROM gameshop4.juegos ORDER BY NOMBRE DESC", nativeQuery = true)
+    List<Juegos>listaJuegosDescendente();
 
     @Transactional
     @Modifying
