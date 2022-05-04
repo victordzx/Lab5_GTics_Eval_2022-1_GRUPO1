87917ef4894802e160bf064f4f546c86a77fff02
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-05-04 13:05:39 -0500

    Serializable (no es atributo ni anotación)

--- Lab5_GTics_Eval_2022-1/src/main/java/edu/pucp/gtics/lab5_gtics_20221/entity/User.java
+++ Lab5_GTics_Eval_2022-1/src/main/java/edu/pucp/gtics/lab5_gtics_20221/entity/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 @Entity
 @Table(name="usuarios")
-public class User {
+public class User implements Serializable{
     @Id
     private int idusuario;
     private String apellidos;
